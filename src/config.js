7df57582403c6bc15ae6a1c2479e2d47832622f3
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-12-23 09:18:02 +0530

    Creating Chat Application

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    appId: "12342f64039347d",
+    apiKey: "e0c0aff92ab750a429745c55e1e19db1e46db25b",
+    GUID: "projectgroup" 
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router } from 'react-router-dom'
+import './index.css';
+import App from './App';
+
+const jsx=(
+    <Router>
+        <App />
+    </Router>
+)
+
+ReactDOM.render(jsx, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
